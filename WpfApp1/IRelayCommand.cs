fa5124c6de47123c3dd00a1d531b6aacc83d691b
hod284 <hod284@naver.com>
2025-11-14 23:53:10 +0900

    inite
    inite

---
+++ WpfApp1/IRelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class IRelayCommand : ICommand
+    {
+        private Action<object> _excute;
+        private Func<object,bool> _canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+          add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public IRelayCommand( Action<object> action ,Func<object,bool> func)
+        { 
+          _excute =action;
+            _canExecute = func;
+        }
+        // 버튼 눌리기전 실행
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null||   _canExecute(parameter);
+        }
+        // 버튼 눌렀을때 실행
+        public void Execute(object parameter)
+        {
+             _excute(parameter);
+        }
+    }
+}
---
+++ WpfApp1/mainview.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    internal class mainview : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Roomnames;
+        public ICommand Joincommand;
+
+        private string _status = "Idle";
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
